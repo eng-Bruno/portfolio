f5aa0b6dbc24b70c3f69ba6a65f4e30e96bad1c6
Bruno Oliveira <bruno.lima@confia.net.br>
2022-12-05 12:17:10 -0300

    esboço página de contato

--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -1,11 +1,53 @@
 import "./Contato.css";
 import Header from "../components/Header";
 
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  Row,
+  Col,
+  Spinner,
+  Progress,
+  Label,
+  CardSubtitle,
+  CardTitle,
+  Input,
+} from "reactstrap";
+
 const Contato = () => {
   return (
-    <div className="home">
+    <div className="contato">
       <Header />
-      <h1>Contato</h1>
+      <Row className="ctt-row">
+        <Col md="8">
+          <Card className="ctt-card">
+            <CardHeader>
+              <CardTitle
+                style={{ color: "white", textAlign: "center", fontSize: "2em" }}
+              >
+                Contato
+              </CardTitle>
+            </CardHeader>
+
+            <CardBody>
+              <Col>
+                <Label>Nome</Label>
+                <Input />
+              </Col>
+              <Col>
+                <Label>Email</Label>
+                <Input type="email" />
+              </Col>
+              <Col>
+                <Label>Corpo de mensagem</Label>
+                <Input type="textarea" rows="10" cols="30" />
+              </Col>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 };
